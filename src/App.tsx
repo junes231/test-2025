48086da02ea28e079a6dc517a8d416482acd310f
junes231 <deyongw3@gmail.com>
2025-06-20 10:29:45 +0800

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import FunnelEditor from './components/FunnelEditor';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <FunnelEditor />
+    </div>
+  );
+}
+
+export default App;
