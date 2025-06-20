5723281a32d1addac9e6f0f226e560cbefca6fa8
junes231 <deyongw3@gmail.com>
2025-06-20 16:00:57 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import FunnelEditor from './components/FunnelEditor';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import EditorPage from './pages/EditorPage';
+import PreviewPage from './pages/PreviewPage';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <FunnelEditor />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/editor" element={<EditorPage />} />
+          <Route path="/preview/:id" element={<PreviewPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
