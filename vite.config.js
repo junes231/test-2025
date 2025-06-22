a23fa35449a39ca10527ec426ee929dbaa69aeed
junes231 <deyongw3@gmail.com>
2025-06-22 14:05:21 +0800

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/funnel-editor/' // 你的 GitHub 仓库名
-});
