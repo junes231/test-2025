1a874b165b39c60d31e98844608b5b5f09ed96da
junes231 <deyongw3@gmail.com>
2025-06-23 00:34:18 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom'; // ✅ 加这个
 import App from './App.tsx';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
