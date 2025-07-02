b81763d3bcd922464043c62e15c0844eb8e39c14
junes231 <deyongw3@gmail.com>
2025-07-02 15:38:55 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
 
@@ -30,8 +30,8 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
   <React.StrictMode>
-    <HashRouter basename="/funnel-editor">
+    <BrowserRouter basename="/funnel-editor">
       <App db={db} />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
