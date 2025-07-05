3b23e0fdb1cfd164b075489a5d475a2beb4c4bec
junes231 <deyongw3@gmail.com>
2025-07-05 16:27:50 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -303,14 +303,7 @@ const FunnelDashboard: React.FC<FunnelDashboardProps> = ({ db, funnels, setFunne
 
   return (
     <div className="dashboard-container">
-     {uid ? (
-  <p style={{ color: 'green' }}>
-    Logged in UID: <code>{uid}</code>
-  </p>
-) : (
-  <p style={{ color: 'gray' }}>Logging in anonymously...</p>
-)}
-      <h2><span role="img" aria-label="funnel">🥞</span> Your Funnels</h2>
+    <h2><span role="img" aria-label="funnel">🥞</span> Your Funnels</h2>
       <div className="create-funnel-section">
         <input
           type="text"
