7256f978d1a541fdc71dda7bd104b78d703bf094
junes231 <deyongw3@gmail.com>
2025-07-06 18:11:23 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useCallback, useRef, ChangeEvent } from 'react';
-FunnelDashboard from './components/FunnelDashboard';
+import FunnelDashboard from './components/FunnelDashboard';
 import FunnelEditor from './components/FunnelEditor';
 import QuizPlayer from './components/QuizPlayer';
 import { useNavigate, useParams, Routes, Route, Link } from 'react-router-dom';
