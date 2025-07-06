5ddf60f90f4a0b49219040134dc78fd7ab6307e9
junes231 <deyongw3@gmail.com>
2025-07-06 14:31:51 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -30,7 +30,7 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <HashRouter> 
-      <AppWrapper db={db} />
+      <App db={db} />
     </HashRouter>
   </React.StrictMode>
 );
