f97f9cb1f0b780b96d24344cc7101af58614b50d
junes231 <deyongw3@gmail.com>
2025-08-16 16:21:41 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -10,14 +10,13 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyA8rJiJnyB6QHgkesFekaRy7f0oftXaF0c',
-  authDomain: 'funnel-editor-netlify.firebaseapp.com',
-  projectId: 'funnel-editor-netlify',
-  storageBucket: 'funnel-editor-netlify.firebasestorage.app',
-  messagingSenderId: '498506838505',
-  appId: '1:498506838505:web:95f20fdfbb260c2b271b78',
-  measurementId: 'G-RVRL76REP7',
-};
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
