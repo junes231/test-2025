17ff1c79dff17720f2e127afc7477af7e5b02428
junes231 <deyongw3@gmail.com>
2025-08-22 16:04:22 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useCallback, useRef, ChangeEvent } from 'react';
 import { getAuth, onAuthStateChanged, User, signOut } from 'firebase/auth';
+import PrivateRoute from './components/PrivateRoute';
 import { useNavigate, useParams, Routes, Route } from 'react-router-dom';
 import {
   collection,
